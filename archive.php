eaec7ed2a735e2d62ba19b39a70d800086833785
admin <admin@admins-MacBook-Pro.local>
2025-07-28 11:51:14 +0100

    Add .site-width styles to aside

--- archive.php
+++ archive.php
@@ -32,6 +32,8 @@ get_header();
 
 <?php 
 
+		$feature_img_url = '';
+
 		if ( ( is_home() ) && ( wp_get_attachment_url( get_post_thumbnail_id($page_id) ) ) ) :
 
 			// get image url attached to page 
